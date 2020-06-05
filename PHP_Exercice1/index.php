c6d1766e4d6830c6b99dc5739b999310770038a8
unknown <jules.gioux@gmail.com>
2020-06-05 15:23:35 +0200

    first commit

---
+++ PHP_Exercice1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Partie 6 Exercice 1</title>
+</head>
+<body>
+    <a href="index.php?lastname=Nemare&firstname=Jean"></a>
+    <?php
+    ?>
+    <p><?php  ?></p>
+</body>
+</html>
